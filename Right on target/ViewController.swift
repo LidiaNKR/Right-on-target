63fb205c3409f7da6058c2993bec989d2e9adc8f
LidiaNKR <77931445+LidiaNKR@users.noreply.github.com>
2022-08-21 13:26:20 +0300

    First setup

--- Right on target/ViewController.swift
+++ Right on target/ViewController.swift
@@ -9,11 +9,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var Slider: UISlider!
+    @IBOutlet var Label: UILabel!
+    
+    var number: Int = 0
+    var round: Int = 0
+    var score: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func checkNumber() {
+        if round == 0 {
+            number = Int.random(in: 1...50)
+            Label.text = String(number)
+            round = 1
+        } else {
+            let numSlider = Int(Slider.value.rounded())
+            if numSlider > number {
+                score += 50 - numSlider + number
+            } else if numSlider < number {
+                score += 50 - number + numSlider
+            } else {
+                score += 50
+            }
+        }
+        if round == 5 {
+            let alert = UIAlertController(title: "Игра окончена", message: "Вы заработали \(score) очков", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Начать заново", style: .default))
+            present(alert, animated: true)
+            round = 1
+            score = 0
+        } else {
+            round += 1
+        }
+        number = Int.random(in: 1...50)
+        Label.text = String(number)
+    }
+    
 }
+
 
